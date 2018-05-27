82e35d3229d783c7c9cf7ecdd064ca00d830c5c1
Romklao <rk120879@gmail.com>
2018-05-27 12:46:10 -0700

    feat: Add condition to none when a book cover is undefined

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -27,39 +27,43 @@ class Book extends Component {
     const { book } = this.props;
     console.log('this.props', this.props)
     let currentShelf = book.shelf;
+    let bookCover = book.imageLinks;
 
     if(currentShelf === undefined) {
       currentShelf = 'none';
     }
 
+    if(bookCover === undefined) {
+      bookCover = 'none';
+    }
+
     return (
       <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193,
-              backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}
-              onClick={() => this.setState({ showBookDetailsModal: true })}>
-            </div>
-            <div className="book-shelf-changer">
-              <select onChange={this.onChangeBookShelf} value={ currentShelf }>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{ book.title }</div>
-          <div className="book-authors">{book.authors ? book.authors[0] : ''}</div>
-        </div>
         {this.state.showBookDetailsModal === true ? (
           <BookDetailsModal
             book={ book }
             hideModal={ this.hideBookDetailsModal }
           />
         ) : (
-          <div></div>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193,
+                backgroundImage: `url(${bookCover.smallThumbnail})` }}
+                onClick={() => this.setState({ showBookDetailsModal: true })}>
+              </div>
+              <div className="book-shelf-changer">
+                <select onChange={this.onChangeBookShelf} value={ currentShelf }>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{ book.title }</div>
+            <div className="book-authors">{book.authors ? book.authors[0] : ''}</div>
+          </div>
         )}
       </li>
     );
