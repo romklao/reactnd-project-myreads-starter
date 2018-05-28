19ff48cb98b299aac46c829fa34518095a152ad2
Romklao <rk120879@gmail.com>
2018-05-28 01:57:15 -0700

    refactor: Use Stateless Functional Component instead of using Class

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Book from './Book';
+import React from 'react'
+import Book from './Book'
 
 /* TODO: This renders all books in a shelf */
-class BookShelf extends Component {
+function BookShelf(props) {
 
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired,
-    changeShelf: PropTypes.func.isRequired
-  }
-
-  render() {
-    const { books } = this.props;
+  const { books } = props
     /* Loop through a shelf to present all books in it */
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ this.props.title }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book, index) => (
-              <Book
-                book={ book }
-                key={ index }
-                changeShelf={this.props.changeShelf}
-              />
-            ))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ props.title }</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book, index) => (
+            <Book
+              book={ book }
+              key={ index }
+              changeShelf={ props.changeShelf }
+            />
+          ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default BookShelf;
+export default BookShelf
