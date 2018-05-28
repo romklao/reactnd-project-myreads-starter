04d68805c0054f2d7c7f4bcdc99b54f97f17b3c5
Romklao <rk120879@gmail.com>
2018-05-28 14:06:16 -0700

    Refactor: Modify the class function to the stateless functional component

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -1,50 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelf from './BookShelf'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 /* TODO: Show the categorized list book */
-class ListBooks extends Component {
+function ListBooks(props) {
 
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired
+  const { books } = props;
+  const bookShelvesNames = ['currentlyReading', 'read', 'wantToRead']
+  const titleNames = {
+    currentlyReading: 'Currently Reading',
+    read: 'Read',
+    wantToRead: 'Want to Read'
   }
 
-  render() {
-    const { books } = this.props;
-    let bookShelvesNames = ['currentlyReading', 'read', 'wantToRead']
-    let titleNames = {
-      currentlyReading: 'Currently Reading',
-      read: 'Read',
-      wantToRead: 'Want to Read'
-    }
-
-    /* Use filter to categorize the shelves and present all books in each shelf */
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {bookShelvesNames.map((bookShelf, index) => (
-            <BookShelf
-              books={books.filter((book) => (book.shelf === bookShelf))}
-              key={ index }
-              title={ titleNames[bookShelf] }
-              changeShelf={ this.props.changeShelf }
-            />
-          ))}
-        </div>
-        <div className="open-search">
-          <Link
-            to='/search'
-            >Add a book
-          </Link>
-        </div>
+  /* Use filter to categorize the shelves and present all books in each shelf */
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+      <div className="list-books-content">
+        {bookShelvesNames.map((bookShelf, index) => (
+          <BookShelf
+            books={books.filter((book) => (book.shelf === bookShelf))}
+            key={ index }
+            title={ titleNames[bookShelf] }
+            changeShelf={ props.changeShelf }
+          />
+        ))}
+      </div>
+      <div className="open-search">
+        <Link
+          to='/search'
+          >Add a book
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default ListBooks
